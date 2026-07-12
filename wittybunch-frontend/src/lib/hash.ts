bb4862dd6aa0c4f3380505a50b0324022f66117b
learninggoalsformations-beep <learninggoalsformations@gmail.com>
2026-07-12 17:03:36 +0200

    Add WittyBunch hash helper

---
+++ wittybunch-frontend/src/lib/hash.ts
@@ -0,0 +1,8 @@
+export function simpleHash(input: string): string {
+  let h = 0;
+  for (let i = 0; i < input.length; i += 1) {
+    h = (h << 5) - h + input.charCodeAt(i);
+    h |= 0;
+  }
+  return Math.abs(h).toString(16).padStart(8, "0");
+}
